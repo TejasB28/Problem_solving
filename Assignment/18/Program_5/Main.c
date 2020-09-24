fda0d94765e509f9fb061433b5acc2216b0c34bc
TejasB28 <sergio.heist4@gmail.com>
2020-09-25 02:05:38 +0530

    Fix Bug

--- Assignment/18/Program_5/Main.c
+++ Assignment/18/Program_5/Main.c
@@ -1,5 +1,5 @@
 /*
-Accept number of rows and number of columns from user and display below
+Accept number of rows and number of columns from user and display below...
 pattern.
 Input : iRow = 4 iCol = 4
 Output : 1 2 3 4
